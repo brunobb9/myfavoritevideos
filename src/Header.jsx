c287770bb4efd5b00ae52e2c54942469e0eb7ad5
brunobb9 <bruno.benny77@outlook.com>
2025-12-03 17:24:45 +0000

    Missing img

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="headerContainer">
-   <img src="./src/assets/imgs/yt-icon.png" className="iconYT"/>
+   <img src="./imgs/yt-icon.png" className="iconYT"/>
         <h1 className="intro" onClick={() => window.location.reload()} style={{cursor:"pointer"}}>
            My Favorite Videos
         </h1>
